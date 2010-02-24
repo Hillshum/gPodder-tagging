c8c090f7d3ecbac42636339d321a6ab3f6a81697
Hillton Shumway <hillshum@gmail.com>
2010-02-23 19:50:48 -0700

    Works now. Still need to figure out why I can't pass the config object when creating the tagger. Also need to add support for non-taggable files

--- src/gpodder/model.py
+++ src/gpodder/model.py
@@ -514,7 +514,9 @@ class PodcastChannel(PodcastModelObject):
         if not item.was_downloaded():
             item.mark_downloaded(save=True)
             self.update_m3u_playlist()
-            tagger.Tagger(config.Config()).update_tag(item)
+            t = tagger.Tagger()
+            t.config = gpodder.config.Config()
+            t.update_tag(item)
 
     def get_all_episodes(self):
         return self.db.load_episodes(self, factory=self.episode_factory)
