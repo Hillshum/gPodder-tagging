55525663e30a2e54cc15249d164a0de9d2dee549
Hillton Shumway <hillshum@gmail.com>
2010-02-23 18:28:59 -0700

    Fix a couple of typos

--- src/gpodder/model.py
+++ src/gpodder/model.py
@@ -514,7 +514,7 @@ class PodcastChannel(PodcastModelObject):
         if not item.was_downloaded():
             item.mark_downloaded(save=True)
             self.update_m3u_playlist()
-            tagger.Tagger(config.Config()).tag(item)
+            tagger.Tagger(config.Config()).update_tag(item)
 
     def get_all_episodes(self):
         return self.db.load_episodes(self, factory=self.episode_factory)
--- src/gpodder/tagger.py
+++ src/gpodder/tagger.py
@@ -28,7 +28,7 @@ class Tagger(object):
 		self.genre = self.config.tag_genre
 		self.title = self.config.tag_title
 		tagvalues = {'channel.title': None,'episode.title':None,'artist':None}
-		tagvalues['channel.title'] = epsisode.channel.title
+		tagvalues['channel.title'] = episode.channel.title
 		tagvalues['episode.title'] = episode.title
 		#tagvalues['artist'] =  #Figure this out too
 
