d5c776cae10acdc4b999698f80657582f3034109
Hillton Shumway <hillshum@gmail.com>
2010-02-23 22:34:33 -0700

    Add some comments about a workaround in model.py and put a proper header in tagger.py

--- src/gpodder/tagger.py
+++ src/gpodder/tagger.py
@@ -1,10 +1,26 @@
+# gPodder - A media aggregator and podcast client
+# Copyright (c) 2010 Hilton Shumway
+#
+# gPodder is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# gPodder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 #This file is a track tagger for gPodder. It will correct the tags on poorly-
 #tagged comment files.
 
-#insert fancy header stuff here
 
 
-from gpodder import config
+
 from gpodder.liblogger import log
 
 
