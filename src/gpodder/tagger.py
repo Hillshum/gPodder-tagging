7e3bf657120455d60073f01b0382e97b7592d8db
Hillton Shumway <hillshum@gmail.com>
2010-02-25 22:48:49 -0700

    Replace some tabs with spaces

--- src/gpodder/tagger.py
+++ src/gpodder/tagger.py
@@ -27,18 +27,18 @@ from gpodder.liblogger import log
 import tagpy
 
 class Tagger(object):
-	"""This class accepts a path to a file and updates the tags for it based 
-	on the attributes of the file (name of podcast, episode number, etc) and 
-	also the user's config.
-	"""
+    """This class accepts a path to a file and updates the tags for it based 
+    on the attributes of the file (name of podcast, episode number, etc) and 
+    also the user's config.
+    """
 
-	def __init___(self,config):
-		self.config = config	
+    def __init___(self,config):
+        self.config = config	
 
-	def update_tag(self, episode):
-		filename = episode.local_filename(create=False)
-		if filename is None:
-			log('Cannot update tag of non-existing file')
+    def update_tag(self, episode):
+        filename = episode.local_filename(create=False)
+        if filename is None:
+            log('Cannot update tag of non-existing file')
 			return False
 		try: 
 			tag = tagpy.FileRef(filename).tag()
@@ -46,18 +46,18 @@ class Tagger(object):
 			log('Unable to read tag')
 			return False
 
-		self.artist = self.config.tag_artist
-		self.album = self.config.tag_album
-		self.genre = self.config.tag_genre
-		self.title = self.config.tag_title
-		tagvalues = {'channel.title': None,'episode.title':None,'artist':None}
-		tagvalues['channel.title'] = episode.channel.title
-		tagvalues['episode.title'] = episode.title
-		#tagvalues['artist'] =  #Figure this out too
+        self.artist = self.config.tag_artist
+        self.album = self.config.tag_album
+        self.genre = self.config.tag_genre
+        self.title = self.config.tag_title
+        tagvalues = {'channel.title': None,'episode.title':None,'artist':None}
+        tagvalues['channel.title'] = episode.channel.title
+        tagvalues['episode.title'] = episode.title
+        #tagvalues['artist'] =  #Figure this out too
 
 
-		tag.title = self.title % tagvalues
-		tag.genre = self.genre % tagvalues
-		tag.album = self.album % tagvalues
-		#tag.artist = self.artist % tagvalues # Don't know what to use for this yet
-		return True
+        tag.title = self.title % tagvalues
+        tag.genre = self.genre % tagvalues
+        tag.album = self.album % tagvalues
+        #tag.artist = self.artist % tagvalues # Don't know what to use for this yet
+        return True
